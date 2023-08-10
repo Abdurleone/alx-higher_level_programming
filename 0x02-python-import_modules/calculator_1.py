1bb9190e8ec8afcbd254467f6d3a73e97c9107ae
Abdurleone <abyyyusuf@gmail.com>
2023-08-10 08:13:36 +0300

    module

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -34,3 +34,15 @@ def mul(a, b):
     The return value. a * b
     """
     return (a * b)
+def div(a, b):
+
+    """My division function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
