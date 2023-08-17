288b23f40e95ec83c4cb80ca63d52b51cfcd742e
Abdurleone <abyyyusuf@gmail.com>
2023-08-17 12:27:00 +0300

    edit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     list_keys = list(a_dictionary.keys())
 
--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -25,13 +25,13 @@ void print_python_bytes(PyObject *p)
 
 	printf(" size: %ld\n", size);
 	printf(" trying string: %s\n", string);
-	
+
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
 
-	printf(" first %ld bytes:", limits);
+	printf(" first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
