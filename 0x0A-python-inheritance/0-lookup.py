31ac2e13890ef85d3487028343bad7790bc4c893
Abdurleone <abyyyusuf@gmail.com>
2023-09-11 17:40:16 +0300

    0x0A

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""Defines a function to look for methods & attributes."""
 
 def lookup(obj):
-    """Return list of obj"""
+    """Return list of atributes and methods."""
     return (dir(obj))
