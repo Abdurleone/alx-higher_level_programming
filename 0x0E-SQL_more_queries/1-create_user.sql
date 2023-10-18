3187fc153b58dc3997b6f7cfdfc07e3ac15935b7
Abdurleone <abyyyusuf@gmail.com>
2023-10-18 09:25:24 +0300

    0x0E

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON * . * TO user_0d_1@localhost;
