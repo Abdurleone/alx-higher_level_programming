b541544a471f32197f1e73086cad89b5d64a6116
Abdurleone <abyyyusuf@gmail.com>
2023-11-14 10:57:14 +0300

    0x12

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const myVar = 'Javascript is amazing';
 console.log(myVar);
-=======
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
->>>>>>> 02cfa3c9b5e46f1737081bca13370d88ed7bc280
