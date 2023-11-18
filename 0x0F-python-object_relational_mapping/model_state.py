9fb7296af34cf77354fd404e08663b6c20cda88e
Abdurleone <abyyyusuf@gmail.com>
2023-11-18 19:50:47 +0300

    0x0F

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module to connect to the database
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """defines state class"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = column(String(128), nullable=False)
