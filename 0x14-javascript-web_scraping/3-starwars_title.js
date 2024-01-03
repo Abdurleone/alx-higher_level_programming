86af604f0902597f830d61370d6fbf08644af5c8
Abdurleone <abyyyusuf@gmail.com>
2024-01-03 15:57:43 +0300

    0x14

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+	if(err) {
+		console.log(err);
+	} else if (response.statusCode === 200) {
+		console.log(responseJSON.title);
+	} else {
+		console.log('Error code: ' + response.statusCode);
+	}
+});
