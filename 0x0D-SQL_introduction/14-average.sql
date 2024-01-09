54a9ab740b41257d5004b4de5d38211d29f9ae12
Abdurleone <abyyyusuf@gmail.com>
2024-01-09 17:54:18 +0300

    Fix

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the average score of all records in the table second_table in my MySQL server.
-SELECT AVG(`score`) AS `average`
-FROM `second_table`;
+SELECT AVG(score) AS average
+FROM second_table;
